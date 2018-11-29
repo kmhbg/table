71c7d01717dfb68abfa0a642960a8eadef4b0ed7
Sebastian Nilsson <sebastian.nilsson@melitta.se>
2018-11-29 21:51:42 +0100

    again

---
+++ table.1.php
@@ -0,0 +1,49 @@
+<h1>Masterdata</h1> 
+<a href="welcome.php">Back</a>
+<?php
+header('Content-Type: text/html; charset=UTF-8');
+
+mb_internal_encoding('UTF-8'); 
+mb_http_output('UTF-8'); 
+mb_http_input('UTF-8'); 
+mb_regex_encoding('UTF-8'); 
+
+$host    = "localhost:8889";
+$user    = "root";
+$pass    = "root";
+$db_name = "master";
+
+//create connection
+$connection = mysqli_connect($host, $user, $pass, $db_name);
+
+//test if connection failed
+if(mysqli_connect_errno()){
+    die("connection failed: "
+        . mysqli_connect_error()
+        . " (" . mysqli_connect_errno()
+        . ")");
+}
+
+//get results from database
+$result = mysqli_query($connection,"SELECT * FROM mast");
+$all_property = array();  //declare an array for saving property
+
+//showing property
+echo '<table class="data-table">
+        <tr class="data-heading">';  //initialize table tag
+while ($property = mysqli_fetch_field($result)) {
+    echo '<td>' . $property->name . '</td>';  //get field name for header
+    array_push($all_property, $property->name);  //save those to array
+}
+echo '</tr>'; //end tr tag
+
+//showing all data
+while ($row = mysqli_fetch_array($result)) {
+    echo "<tr>";
+    foreach ($all_property as $item) {
+        echo '<td>' . $row[$item] . '</td>'; //get items using property value
+    }
+    echo '</tr>';
+}
+echo "</table>";
+?>
